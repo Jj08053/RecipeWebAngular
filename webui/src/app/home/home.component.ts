cbef99d3301f9dce8699368edffbfb2b83520c02
ivyzhang2016 <ivy.zhang@stthomas.edu>
2024-05-10 02:49:33 -0500

    Ivy's initial commit

--- webui/src/app/home/home.component.ts
+++ webui/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  panels = [
+    { id: 1, title: 'Cake', image: 'cake.jpg', visible: true },
+    { id: 2, title: 'Chili', image: 'chili.jpg', visible: false },
+    { id: 3, title: 'Penne', image: 'penne.jpg', visible: false }
+  ];
+
+  showPanel(panel) {
+    this.panels.forEach(p => p.visible = false);
+    panel.visible = true;
+  }
+
+  constructor(public router:Router) {
+
+  }
+  
+  goToRecipe(id:number) {
+    this.router.navigateByUrl('/recipe/'+id);
+  }
 
 }
